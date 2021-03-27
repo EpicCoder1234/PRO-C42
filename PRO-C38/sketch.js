84bbdbc608f3ab36b9b5333edf6b65c495035e9c
EpicCoder1234 <hrudayiitb@gmail.com>
2021-03-26 21:06:13 -0700

    commit

---
+++ PRO-C38/sketch.js
@@ -0,0 +1,47 @@
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+var backgroundImage, bg;
+var player_standing,player_flying, player;
+var ground;
+var rand;
+var rand2;
+var missileImage,missileGroup;
+var gameover;
+var coinImage,coinGroup;
+var score=0
+
+
+function preload(){
+  backgroundImage = loadImage("bg.jpg")
+  player_standing = loadImage("player_standing.png");
+}
+
+function setup() { 
+  createCanvas(displayWidth, displayHeight);
+  bg = createSprite(displayWidth-200,displayHeight/2-200,1000,displayHeight);
+  bg.scale = 3;
+  bg.addImage("bg", backgroundImage);
+  
+  player = createSprite(100,200,20,30);
+  player.scale = 0.3;
+  player.addImage("player_standing",player_standing);
+
+
+  camera.position.x = player.x;
+  camera.position.y = player.y;
+
+
+
+}
+function draw() { 
+  if(keyDown("right")){
+    player.x = player.x+10;
+  }
+
+  
+
+  drawSprites();
+  console.log(player.y);
+}
+
